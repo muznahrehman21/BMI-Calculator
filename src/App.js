9bed70ff1e4242024fb64012514228de1133a62e
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-05 23:34:29 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='container'>
+        <h2>BMI Calculator</h2>
+        <form>
+<label> Weight (lbs)  </label>
+        </form>
+      </div>
+
+
     </div>
   );
 }
